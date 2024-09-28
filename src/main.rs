3d05b492d30ae7f3d02d946ff6d3a027dc659ea5
cielaton <vuwin24680@gmail.com>
2024-09-28 22:23:30 +0700

    Added the connect function to postgresql.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use database::connect::Database;
+
+mod database;
+
+#[tokio::main]
+async fn main() {
+    let db = Database::connect_database().await;
+    println!("{:#?}", db.unwrap().connection.await);
 }
